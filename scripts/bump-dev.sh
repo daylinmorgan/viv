5308f69b4e40724391497c905100fef81d89417f
Daylin Morgan <daylinmorgan@gmail.com>
2023-05-27 16:42:48 -0500

    fix: use the correct file name

--- scripts/bump-dev.sh
+++ scripts/bump-dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 TAG=$(git describe --tags --always --dirty=-dev)
 VERSION="${TAG#v}"
-if [[ "$(git diff --name-only HEAD HEAD~1)" == *"./src/viv/viv.py"* ]]; then
+if [[ "$(git diff --name-only HEAD HEAD~1)" == *"src/viv/viv.py"* ]]; then
 	sed -i "s/__version__ = \".*\"/__version__ = \"$VERSION\"/g" src/viv/viv.py
 fi
