29d9d965b319d03d70cbf7f06fd87522fbf65fbc
Daylin Morgan <daylinmorgan@gmail.com>
2023-10-18 09:29:00 -0500

    build: task.mk by default

--- .task.cfg.mk
+++ .task.cfg.mk
@@ -3,4 +3,4 @@ PHONIFY=1
 HELP_SEP={a.b_blue}>>>{a.end}
 
 -include .task.mk
-$(if $(filter help,$(MAKECMDGOALS)),$(if $(wildcard .task.mk),,.task.mk: ; curl -fsSL https://raw.githubusercontent.com/daylinmorgan/task.mk/v23.1.2/task.mk -o .task.mk))
+$(if $(wildcard .task.mk),,.task.mk: ; curl -fsSL https://raw.githubusercontent.com/daylinmorgan/task.mk/v23.1.2/task.mk -o .task.mk)
