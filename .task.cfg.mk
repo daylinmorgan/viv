726cef30921c5f603b2e0384a461dd7d07485264
Daylin Morgan <daylinmorgan@gmail.com>
2023-11-30 17:03:17 -0600

    build: why make when we can do

--- .task.cfg.mk
+++
@@ -1,6 +0,0 @@
-USAGE={a.bold}{a.cyan} viv tasks{a.end}:\n
-PHONIFY=1
-HELP_SEP={a.b_blue}>>>{a.end}
-
--include .task.mk
-$(if $(wildcard .task.mk),,.task.mk: ; curl -fsSL https://raw.githubusercontent.com/daylinmorgan/task.mk/v23.1.2/task.mk -o .task.mk)
