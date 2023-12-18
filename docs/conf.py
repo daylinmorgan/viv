3673e4940c99f125df838aa2f0d0a5b344511db4
Daylin Morgan <daylinmorgan@gmail.com>
2023-12-18 12:27:44 -0600

    docs: furo -> shibuya

--- docs/conf.py
+++ docs/conf.py
@@ -2,12 +2,15 @@ project = "Viv"
 copyright = "2023, Daylin Morgan"
 author = "Daylin Morgan"
 
-extensions = ["myst_parser"]
+extensions = ["myst_parser", "sphinx_copybutton"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_extra_path = ["viv.py"]
 
-html_theme = "furo"
-html_static_path = ["_static"]
+
+html_theme = "shibuya"
+# html_static_path = ["_static"]
 html_logo = "../assets/logo.svg"
+
+html_theme_options = {"github_url": "https://github.com/daylinmorgan/viv"}
